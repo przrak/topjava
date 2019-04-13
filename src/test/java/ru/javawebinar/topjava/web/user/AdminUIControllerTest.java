9d4da7ddc94b7e964bc343780801549a21318bbc
przrak <przrak@gmail.com>
2019-04-13 12:25:08 +0700

    fix test HW8

--- src/test/java/ru/javawebinar/topjava/web/user/AdminUIControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminUIControllerTest.java
@@ -8,23 +8,21 @@ import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.javawebinar.topjava.UserTestData.*;
-import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 class AdminUIControllerTest extends AbstractControllerTest
 {
-    private static final String REST_URL = AdminRestController.REST_URL + '/';
+    private static final String REST_URL = "/ajax/admin/users/";
 
     @Test
-    void testUpdate() throws Exception
+    void testUpdateActiveState() throws Exception
     {
         User updated = new User(USER);
         updated.setEnabled(true);
         updated.setRoles(Collections.singletonList(Role.ROLE_USER));
-        mockMvc.perform(get(REST_URL + USER_ID + "?enabled=true")
+        mockMvc.perform(post(REST_URL + USER_ID + "?enabled=true")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
 
